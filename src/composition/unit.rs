443610eeadd07ec41702b3dbc60ba64d8c9c2cd8
Tim-Jonas Wechler <code.timjonaswechler@icloud.com>
2025-11-06 11:39:40 +0100

    update.

--- src/composition/unit.rs
+++
@@ -1,3 +0,0 @@
-pub trait UnitScale {
-    fn scale() -> f64;
-}
--- src/quantities/luminosity.rs
+++
@@ -1,6 +0,0 @@
-#![allow(non_snake_case)]
-
-use crate::features::DefaultFloat;
-use crate::{define_quantity, define_units};
-
-define_quantity!(Luminosity); // ML²T⁻³
